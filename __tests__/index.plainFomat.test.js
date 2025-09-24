151109fc3c41bd55f51f97ad4238888eefdf8b19
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-09-24 12:32:22 +0300

    проведена работа с тестами

--- __tests__/index.plainFomat.test.js
+++ __tests__/index.plainFomat.test.js
@@ -3,6 +3,11 @@ import path from 'path'
 import fs from 'fs'
 import buildDiffTree from '../src/diffBuilder/index.js'
 import plain from '../formatters/plain.js'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
