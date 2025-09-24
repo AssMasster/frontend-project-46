fc03bcf4e958c5d6c2f368bd3afad3a55affce56
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-09-24 12:34:14 +0300

    фикс линтером

--- __tests__/index.jsonFormatOutput.test.js
+++ __tests__/index.jsonFormatOutput.test.js
@@ -3,11 +3,11 @@ import path from 'path'
 import fs from 'fs'
 import buildDiffTree from '../src/diffBuilder/index.js'
 import json from '../formatters/json.js'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
--- __tests__/index.yml.test.js
+++ __tests__/index.yml.test.js
@@ -4,11 +4,11 @@ import fs from 'fs'
 import yml from 'js-yaml'
 import buildDiffTree from '../src/diffBuilder/index.js'
 import stylish from '../formatters/stylish.js'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
